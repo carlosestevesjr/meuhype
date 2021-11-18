bf8fd39697e9d690d206a0a0b5e3c4862b1ee147
carlosestevesjr <carlosestevesjr0@gmail.com>
2021-11-18 00:15:06 -0300

    teste deploy

--- resources/views/admin/pages/news/index.blade.php
+++ resources/views/admin/pages/news/index.blade.php
@@ -19,7 +19,6 @@
     </div>
 </div>
 
-
 @if(Session::has('message'))
     <div class="alert alert-success alert-dismissible" role="alert">
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
